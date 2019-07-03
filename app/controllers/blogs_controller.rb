1f06664a0237c90045d48d65f36eef82fb3c0df3
EC2 Default User <ec2-user@ip-172-31-18-161.ap-southeast-1.compute.internal>
2019-07-03 10:18:58 +0000

    user機能本格作成前まで

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,17 @@
+class BlogsController < ApplicationController
+    def index
+        @blogs = Blog.order("created_at DESC").page(params[:page]).per(5)
+    end
+    
+    def new
+    end
+    
+    def create
+        Blog.create(blog_params)
+    end
+    
+    private
+    def blog_params
+        params.permit(:user_name, :image, :text)
+    end
+end
