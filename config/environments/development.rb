7c79fce95fc41779c3d5ca9b13fc30c331de7628
EC2 Default User <ec2-user@ip-172-31-18-161.ap-southeast-1.compute.internal>
2019-07-04 08:25:54 +0000

    user機能signup表示できた

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'https://53d511034171492591668ac79ea97ce0.vfs.cloud9.ap-southeast-1.amazonaws.com' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root 'blogs#index'
+    root to: 'top#index'
     get 'blogs' => 'blogs#index'
     get 'blogs/new' => 'blogs#new'
     post 'blogs' => 'blogs#create'
