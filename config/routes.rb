5f4768f433672afcb686e33f7a0d0b58102253ad
EC2 Default User <ec2-user@ip-172-31-18-161.ap-southeast-1.compute.internal>
2019-08-12 09:24:27 +0000

    ユーザーページを編集できるようにしようまで完了。ただし、完璧な編集機能ではない

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: 'top#index'
-    resources :users, only: [:show]
+    resources :users, only: [:show, :edit, :update]
     get 'blogs' => 'blogs#index'
     get 'blogs/new' => 'blogs#new'
     post 'blogs' => 'blogs#create'
