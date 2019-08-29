e5347f7d4ca582373a53750e2b36f5ee746bfb55
EC2 Default User <ec2-user@ip-172-31-18-161.ap-southeast-1.compute.internal>
2019-08-29 10:58:56 +0000

    いいね機能も搭載完了！

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -44,3 +44,49 @@ class BlogsController < ApplicationController
         params.require(:blog).permit(:user_name, :title, :text, :image)
     end
 end
+class BlogsController < ApplicationController
+    def index
+        @blogs = Blog.order("created_at DESC").page(params[:page]).per(5)
+      
+    end
+    
+    def detailpage
+        @blog = Blog.find(params[:id])
+    end
+    
+    def new
+        @blog = Blog.new
+    end
+    
+    def show
+        @blog = Blog.find(params[:id])
+    end
+    
+    def create
+        Blog.create(user_name: blog_params[:user_name], title: blog_params[:title], text: blog_params[:text], user_id: current_user.id, image: blog_params[:image])
+       
+    end
+    
+    def destroy
+       blog = Blog.find(params[:id])
+       if blog.user_id == current_user.id
+           blog.destroy
+       end
+    end
+    
+    def edit
+        @blog = Blog.find(params[:id])
+    end
+    
+    def update
+        blog = Blog.find(params[:id])
+        if blog.user_id == current_user.id
+            blog.update(blog_params)
+        end
+    end
+    
+    private
+    def blog_params
+        params.require(:blog).permit(:user_name, :title, :text, :image)
+    end
+end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -8,6 +8,9 @@ class Blog < ApplicationRecord
     belongs_to :user
     has_one :feed_content, as: :content, dependent: :destroy
 
+    has_many :likes
+    has_many :liked_users, through: :likes, source: :user
+
     private
     def create_feed_content
         self.feed_content = FeedContent.create(user_id: user_id, updated_at: updated_at)
