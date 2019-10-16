f611e3f880e843bd87df5d6452777219c32d53a2
Fox-ag <lexussc430qualia@gmail.com>
2019-10-16 10:09:17 +0000

    プロフィール編集がこれまでは名前だけであったものの、ユーザーネームとかも編集できるようにした。また、プロフィール登録できていなくてもプロフィールタブを押せるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
     
     private
     def update_params
-        params.require(:user).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :image)
+        params.require(:user).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :image, :nickname, :birthdate, :country, :city)
     end
 end
