ab87ba48f2b7749e1efedd38ad4472905ed32445
Fox-ag <lexussc430qualia@gmail.com>
2020-01-28 11:31:31 +0000

    upate for upload to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Emotion.create(feeling:'good')
+Emotion.create(feeling:'okey')
+Emotion.create(feeling:'bad')
