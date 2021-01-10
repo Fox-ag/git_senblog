0c50bae9ac6ac1082c9c7bd18a580ccec34b638d
Fox-ag <lexussc430qualia@gmail.com>
2021-01-10 22:38:40 +0000

    スマホ用html css javaの更新。データベースに変更はない

--- app/assets/javascripts/arkchive.js
+++ app/assets/javascripts/arkchive.js
@@ -1,5 +1,5 @@
-$(window).load(function() {
- 
+// $(window).load("turbolinks:load",function() {
+document.addEventListener("turbolinks:load", function() {
   // 一旦hide()で隠してフェードインさせる
   $('.div-blog-block').hide().fadeIn(1500);
   $('.div-for-blog').hide().fadeIn(1500);
@@ -11,5 +11,44 @@ $(window).load(function() {
   $('.profile-area').hide().fadeIn(1500);
   $('.show-profile-area').hide().fadeIn(2000);
   
- 
+});
+
+// $(function () {
+document.addEventListener("turbolinks:load",function() {
+  $('#searchbox-show').click(function(){
+   $('#searchbox-modal').fadeIn(); 
+  });
+  
+  $('.sp_out_box').click(function(){
+   $('#searchbox-modal').fadeOut(); 
+  });
+  
+});
+
+document.addEventListener("turbolinks:load",function() {
+  $('.sp_droptab').click(function(){
+   $('.sp_drop_tab_section').fadeIn(); 
+  });
+  
+  $('.sp_timeline_mainbody').click(function(){
+   $('.sp_drop_tab_section').fadeOut();
+  });
+  
+  $('.sp_drop_tab_close').click(function(){
+   $('.sp_drop_tab_section').fadeOut();
+  });
+});
+
+document.addEventListener("turbolinks:load",function() {
+  $('.sp_login_buttons').click(function(){
+   $('.sp_login_loginarea').fadeIn(); 
+  });
+  
+  $('.sp_login_buttons_two').click(function(){
+   $('.sp_login_loginarea').fadeIn(); 
+  });
+  
+  $('.sp_login_close_box').click(function(){
+   $('.sp_login_loginarea').fadeOut();
+  });
 });
