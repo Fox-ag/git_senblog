9839788331f8e47c5436516780c4406b92099c14
Fox-ag <lexussc430qualia@gmail.com>
2021-01-14 21:23:54 +0000

    loading gif added althouge error on develiping environment

--- app/assets/javascripts/arkchive.js
+++ app/assets/javascripts/arkchive.js
@@ -1,3 +1,28 @@
+window.onload = function(){  // ローディング画面をフェードアウトさせる
+    $(function() {
+        $("#loading").fadeOut();
+    });
+}
+     
+$(function() {   //ロード中はコンテンツの高さをページの高さに合わせる
+    var h = $(window).height();
+    $('#contents').css('display','none');
+    $('#loader-bg ,#loader').height(h).css('display','block');
+});
+      
+// $(window).load(function () {  //全ての読み込みが完了したら実行する
+document.addEventListener("turbolinks:load", function() {
+    $('#loader-bg').delay(900).fadeOut(800);
+    $('#loader').delay(600).fadeOut(300);
+    $('#contents').css('display', 'block');
+});
+ 
+$(function(){  //10秒たったらロードを終わらせる
+    setTimeout('stopload()',10000);
+});
+
+
+
 // $(window).load("turbolinks:load",function() {
 document.addEventListener("turbolinks:load", function() {
   // 一旦hide()で隠してフェードインさせる
